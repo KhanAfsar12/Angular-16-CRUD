69a8741eb7cf4ff8286c1b49696208468d51177c
KhanAfsar12 <ka484564@gmail.com>
2024-10-30 10:24:19 +0530

    added div

--- src/app/creditcards/creditcards.component.ts
+++ src/app/creditcards/creditcards.component.ts
@@ -1,4 +1,51 @@
 import { Component } from '@angular/core';
+import { CreditCard } from '../models/credit-card';
+import { MatTableDataSource } from '@angular/material/table';
+
+const TABLE_DATA: CreditCard[] = [
+  {
+    id: 1,
+    name: 'Bank Of America',
+    description: 'Bank Of America offers customers with various options.',
+    bankName: 'Bank Of America',
+    maxCredit: 3000,
+    interestRate: 10,
+    active: true,
+    recommendedScore: '700-900',
+    annualFee: 4,
+    termsAndConditions: 'Following are the terms & conditions',
+    createdDate: '2023-31-08',
+    updateDate: '2023-31-08',
+  },
+  {
+    id: 2,
+    name: 'Bank Of America',
+    description: 'Bank Of America offers customers with various options.',
+    bankName: 'Bank Of America',
+    maxCredit: 3000,
+    interestRate: 10,
+    active: true,
+    recommendedScore: '700-900',
+    annualFee: 4,
+    termsAndConditions: 'Following are the terms & conditions',
+    createdDate: '2023-31-08',
+    updateDate: '2023-31-08',
+  },
+  {
+    id: 3,
+    name: 'Bank Of America',
+    description: 'Bank Of America offers customers with various options.',
+    bankName: 'Bank Of America',
+    maxCredit: 3000,
+    interestRate: 10,
+    active: true,
+    recommendedScore: '700-900',
+    annualFee: 4,
+    termsAndConditions: 'Following are the terms & conditions',
+    createdDate: '2023-31-08',
+    updateDate: '2023-31-08',
+  }
+]
 
 @Component({
   selector: 'app-creditcards',
@@ -6,5 +53,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./creditcards.component.scss']
 })
 export class CreditcardsComponent {
+  displayColumns = ["select", "id", "name", "description", "bankName", "maxCredit", "interestRate", "active", "recommendedScore"];
 
+  dataSource = new MatTableDataSource(TABLE_DATA);
 }
--- src/app/creditcards/creditcards.module.ts
+++ src/app/creditcards/creditcards.module.ts
@@ -7,6 +7,9 @@ import { AddComponent } from './add/add.component';
 import { ViewComponent } from './view/view.component';
 import { EditComponent } from './edit/edit.component';
 import { DeleteComponent } from './delete/delete.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+
 
 
 @NgModule({
@@ -19,7 +22,9 @@ import { DeleteComponent } from './delete/delete.component';
   ],
   imports: [
     CommonModule,
-    CreditcardsRoutingModule
+    CreditcardsRoutingModule,
+    MatCardModule,
+    MatTableModule
   ]
 })
 export class CreditcardsModule { }
